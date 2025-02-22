aa859de0e35ad4d944e9d63034fe0230d34dd931
HASITHAmwp <hasithamiureanga@gmail.com>
2025-02-22 07:18:52 -0800

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~CIlWjJqT#WV_8kUZzstwACtCICGwYaJ6FSC_3X2JT6x1u9KJV3vY",
 PORT: process.env.PORT || "8000"
 };
